300188d3e326b619a4f70941cd1f20e7a1508345
NicolaVerlato <nicola.verlato00@gmail.com>
2023-06-21 12:11:20 +0200

    logic done

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,11 +8,48 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if(action.type === 'ADD'){
-        const updateItems = state.items.concat(action.item);
         const newTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+        let updatedItems;
+        
+        // se ad esempio il sushi esiste già nel carrello
+        // cambiamo solo il totale di quell'articolo senza scriverlo due volte  
+        if (existingCartItem){
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
+
         return {
-            items: updateItems,
+            items: updatedItems,
             totalAmount: newTotalAmount
+        }
+    }
+
+    if (action.type === 'REMOVE'){
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price
+        let updatedItems;
+
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id)
+        } else {
+            const updatedItem = {...existingItem, amount: existingItem.amount - 1}
+            updatedItems = [...state.items]
+            updatedItems[existingCartItemIndex] = updatedItem
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
         }
     }
     return defaultCartState
